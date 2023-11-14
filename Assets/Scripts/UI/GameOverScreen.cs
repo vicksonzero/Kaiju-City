d0cefa1e939199fb89ceffcb17ecf0ef8c17b1e1
Dickson Chui <dicksonZero@gmail.com>
2023-11-14 16:37:03 +0800

    Add delayed restart button

--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
 {
     public RectTransform winLabel;
+    public RectTransform winOkButton;
     public RectTransform loseLabel;
+    public RectTransform loseOkButton;
 
     // Start is called before the first frame update
     void Start()
     {
         winLabel.gameObject.SetActive(false);
+        winOkButton.gameObject.SetActive(false);
         loseLabel.gameObject.SetActive(false);
+        loseOkButton.gameObject.SetActive(false);
     }
 
     public void Show(bool isWin)
@@ -21,6 +26,9 @@ public class GameOverScreen : MonoBehaviour
 
         if (gameObject) gameObject.SetActive(true);
         winLabel.gameObject.SetActive(isWin);
+        DOVirtual.DelayedCall(2f, () => winOkButton.gameObject.SetActive(true));
+
         loseLabel.gameObject.SetActive(!isWin);
+        DOVirtual.DelayedCall(2f, () => loseOkButton.gameObject.SetActive(true));
     }
 }
