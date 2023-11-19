0bc94c082cfbbd6feb3b7b4b01d6a7188b2a16d0
Dickson Chui <dicksonZero@gmail.com>
2023-11-19 20:43:34 +0800

    Update credits

--- Assets/Scripts/Player/GiantMelee.cs
+++ Assets/Scripts/Player/GiantMelee.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using StarterAssets;
-using UnityEditor.Animations;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class GiantMelee : MonoBehaviour
 {
