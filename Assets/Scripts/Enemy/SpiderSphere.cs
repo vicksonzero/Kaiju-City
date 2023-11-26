fef6edc2f5930378e19dc66c62df146de003028d
Dickson Chui <dicksonZero@gmail.com>
2023-11-26 23:39:17 +0800

    Get colliders for spider legs

--- Assets/Scripts/Enemy/SpiderSphere.cs
+++ Assets/Scripts/Enemy/SpiderSphere.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using StarterAssets;
 using UnityEngine;
 
 public class SpiderSphere : ADeathHandler
@@ -23,6 +24,17 @@ public class SpiderSphere : ADeathHandler
         Close();
     }
 
+    void TryOpen()
+    {
+        if (FindObjectOfType<ThirdPersonTankController>(false))
+        {
+            Open();
+            return;
+        }
+
+        DOVirtual.DelayedCall(Random.Range(openInterval.x, openInterval.y), TryOpen);
+    }
+
     void Open()
     {
         sphere.DOScale(Vector3.one, 0.5f);
@@ -34,7 +46,7 @@ public class SpiderSphere : ADeathHandler
     {
         sphere.DOScale(Vector3.zero, 0.5f);
         _health.canTakeDamage = false;
-        DOVirtual.DelayedCall(Random.Range(openInterval.x, openInterval.y), Open);
+        DOVirtual.DelayedCall(Random.Range(openInterval.x, openInterval.y), TryOpen);
     }
 
     public override void Die(Vector3? hitPoint, Vector3? hitNormal, Vector3? hitImpulse)
